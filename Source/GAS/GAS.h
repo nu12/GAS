be096f5cd4334969e086270ce81b49897eb6cd95
Alysson A. Costa <alysson.avila.costa@gamil.com>
2020-10-22 17:51:28 -0300

    Add input enum

--- Source/GAS/GAS.h
+++ Source/GAS/GAS.h
@@ -3,3 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+/** 
+* Abilities will be mapped & bound to these input values
+*/
+UENUM(BlueprintType)
+enum class EGASAbilityInputID : uint8
+{
+	None,	// Part of the system
+	Confirm,// Part of the system
+	Cancel,	// Part of the system
+	
+	Punch	// Custom
+};
