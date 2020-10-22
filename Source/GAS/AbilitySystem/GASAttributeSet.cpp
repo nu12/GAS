fd840840e385d020dfab5e659b5a7ccf900aade2
Alysson A. Costa <alysson.avila.costa@gamil.com>
2020-10-22 17:52:13 -0300

    Add Attributes (Health, Stamina, AttackPower) & Notifiers

--- Source/GAS/AbilitySystem/GASAttributeSet.cpp
+++ Source/GAS/AbilitySystem/GASAttributeSet.cpp
@@ -2,4 +2,32 @@
 
 
 #include "GASAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UGASAttributeSet::UGASAttributeSet()
+{
+
+}
+
+void UGASAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASAttributeSet, AttackPower, COND_None, REPNOTIFY_Always);
+}
+
+void UGASAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, Health, OldHealth);
+}
+
+void UGASAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, Stamina, OldStamina);
+}
+
+void UGASAttributeSet::OnRep_AttackPower(const FGameplayAttributeData& OldAttackPower)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASAttributeSet, AttackPower, OldAttackPower);
+}
--- Source/GAS/AbilitySystem/GASAttributeSet.h
+++ Source/GAS/AbilitySystem/GASAttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GAS/AbilitySystem/GASAbilitySystemComponent.h"
 #include "GASAttributeSet.generated.h"
+
+// All macros using in this class come from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +22,37 @@ class GAS_API UGASAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UGASAttributeSet();
+
+	// Replication will send information about attributes change to other actors
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/**
+	* Attributes are set below
+	*/
+
+	// Health
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Health)
+		FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UGASAttributeSet, Health);
+
+	UFUNCTION()
+		virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	// Stamina
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Stamina)
+		FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UGASAttributeSet, Stamina);
+
+	UFUNCTION()
+		virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
+
+	// AttackPower
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_AttackPower)
+		FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UGASAttributeSet, AttackPower);
+
+	UFUNCTION()
+		virtual void OnRep_AttackPower(const FGameplayAttributeData& OldAttackPower);
 };
